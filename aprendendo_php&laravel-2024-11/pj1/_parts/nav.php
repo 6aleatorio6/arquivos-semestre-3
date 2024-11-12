c22eb19258a06c5c1d1cdf74f97ea2aad7fde34d
geleia <leonardolfelix12@gmail.com>
2024-11-11 21:31:39 -0300

    esqueleto do pj1 php feito

---
+++ aprendendo_php&laravel-2024-11/pj1/_parts/nav.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/index.php">PAIA</a>
+
+
+        <ul class="nav justify-content-end">
+            <li class="nav-item  ">
+                <a class="nav-link" href="/login.php">LOGIN</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="/cadastro.php">CADASTRO</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ aprendendo_php&laravel-2024-11/pj1/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>home php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="container ">
+    <?php include('_parts/nav.php') ?>
+
+    <h1>hello, criatura estranha!</h1>
+    <h6>parece que você não está logado</h6>
+
+
+    <div class="mt-5 ">
+        <h4>pessoas cadastradas</h4>
+
+        <table class="table border-1 border">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
